93e698f7f3ff0f73b12a459dc4c81f204fdca808
omkar manjrekar <omiimanj@gmail.com>
2026-08-31 17:14:31 +0530

    Dockerize MERN application with Docker Compose

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
+
     try {
         const connectionParams = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         };
-        const useDBAuth = process.env.USE_DB_AUTH || false;
-        if(useDBAuth){
+
+        const useDBAuth = process.env.USE_DB_AUTH === "true";
+
+        if (useDBAuth) {
             connectionParams.user = process.env.MONGO_USERNAME;
             connectionParams.pass = process.env.MONGO_PASSWORD;
         }
+
         await mongoose.connect(
-           process.env.MONGO_CONN_STR,
-           connectionParams
+            process.env.MONGO_CONN_STR,
+            connectionParams
         );
+
         console.log("Connected to database.");
+
     } catch (error) {
         console.log("Could not connect to database.", error);
+        process.exit(1);
     }
 };
